8a54068f1df46d3acc3028a205e109ba5644292e
rdhmdhl <reid@wearthefewture.com>
2024-02-19 17:50:13 -0800

    refactor conversations, require token, remove infinite scroll logic

--- src/components/feed/ConversationsFeed.jsx
+++ src/components/feed/ConversationsFeed.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import './conversationFeed.css';
 import axios from 'axios';
-import InfiniteScroll from 'react-infinite-scroll-component';
-// import { AuthContext } from '../../context/AuthContext';
 import React from 'react';
 import ReactLoading from 'react-loading';
 import PropTypes from 'prop-types';
@@ -14,10 +12,7 @@ export default function ConversationsFeed({
     user
 }) {
   const [conversations, setConversations] = useState([]);
-  const [page, setPage] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
   const [allConversationsLoaded, setAllConversationsLoaded] = useState(false); // New state variable
-  const [initialLoad, setInitialLoad] = useState(true);
   const [showPopup, setShowPopup] = useState(false);
   const [popupMessage, setPopupMessage] = useState('');
 
@@ -27,91 +22,56 @@ export default function ConversationsFeed({
     setShowPopup(true);
   }
 
-
   const fetchConversations = async () => {
     let res;
     try {
+        const token = localStorage.getItem("token");
         // fetch conversations
-        res = await axios.get(`${config.apiUrl}/conversations/${user._id}?page=${page}`);
-
-      if (res.data.length === 25) {
-        setPage(prevPage => prevPage + 1);
-        setConversations((conversations) => [...conversations, ...res.data]);
-        setHasMore(true);
-      } if (res.data.length < 25 && res.data.length > 0){
-        setConversations((conversations) => [...conversations, ...res.data]);
-        setHasMore(false);
-        setAllConversationsLoaded(true);
-      } else if (res.data.length === 0 ) { 
-        setHasMore(false);
-        setAllConversationsLoaded(true);
-      }
+        res = await axios.get(`${config.apiUrl}/conversations/${user._id}`, {
+          headers: {
+              'x-auth-token': token
+          }
+        });
+        if(res){
+          setConversations(res.data);
+          setAllConversationsLoaded(true);
+        }
     } catch (error) {
       await popupStaus("An error occured when fetching conversations. Please try again later.")
     }
 }
 
-
 useEffect(() => {
-  if (initialLoad) {
-    fetchConversations();
-    setInitialLoad(false);
-  }
-}, [hasMore]);
-
-
+  fetchConversations();
+}, [])
 
 return (
-      <div className='conversations-feed' style={{backgroundColor: 'rgb(21, 24, 25)'}}>
-        <Popup isPopupOpen={showPopup} message={popupMessage} button1Text="Close" button1Action={() => setShowPopup(false)} />
-          <div className="feedWrap">
-            {!allConversationsLoaded ? (
-              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10rem', marginBottom: '20px' }}>
-                <ReactLoading type={'balls'} color={'white'} height={'20%'} width={'20%'} />
-              </div>
-            ) : (
-              <InfiniteScroll
-              className="infinite-scroll-component" style={{ height: 'auto', overflow: 'visible' }}
-              dataLength={conversations.length}
-              next={fetchConversations}
-              hasMore={hasMore}
-              // loader={
-              // <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px', marginBottom: '20px' }}>
-              //   <ReactLoading type={'bars'} color={'#000'} height={'50px'} width={'50px'} />
-              //   </div>}
-              page={page}
-              endMessage={
-                <div style={{ textAlign: 'center', margin: '2rem' }}>
-                <p style={{ fontWeight: '100', color: 'var(--gray)' }}>
-                  We prioritize your security. All messages are encrypted end-to-end for maximum privacy and security.
-                </p>
-                <br/>
-                <p style={{ fontWeight: '100', color: 'var(--gray)' }}>
-                  Send or receive a gift to start a conversation.
-                </p>
-              </div>
-              }
-              // TODO --> write a refresh fuction
-              // pullDownToRefresh
-              // pullDownToRefreshContent={
-              //   <h3 style={{textAlign: 'center'}}>&#8595; Pull down to refresh</h3>
-              // }
-              // releaseToRefreshContent={
-              //   <h3 style={{textAlign: 'center'}}>&#8593; Release to refresh</h3>
-              // }
-              // refreshFunction={this.refresh}
-              
-              >
-            {/* <Share/> */}
-            {conversations.map((c, index) => (
-              <Conversation key={c._id + '-' + index} conversation={c} socket={socket}/>
-            ))}
-
-            </InfiniteScroll>
-            )}
-              </div>
+  <div className='conversations-feed' style={{backgroundColor: 'rgb(21, 24, 25)'}}>
+    <Popup isPopupOpen={showPopup} message={popupMessage} button1Text="Close" button1Action={() => setShowPopup(false)} />
+      <div className="feedWrap">
+        {!allConversationsLoaded ? (
+          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10rem', marginBottom: '20px' }}>
+            <ReactLoading type={'balls'} color={'white'} height={'20%'} width={'20%'} />
           </div>
-      )
+        ) : (
+          <div className="conversations">
+            {conversations.map((c, index) => (
+                <Conversation key={c._id + '-' + index} conversation={c} socket={socket}/>
+                ))}
+              <div style={{ textAlign: 'center', margin: '2rem' }}>
+              <p style={{ fontWeight: '100', color: 'var(--gray)' }}>
+                We prioritize your security. All messages are encrypted end-to-end for maximum privacy and security.
+              </p>
+              <br/>
+              <p style={{ fontWeight: '100', color: 'var(--gray)' }}>
+                Send or receive a gift to start a conversation.
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+  </div>
+  )
 }
 
 ConversationsFeed.propTypes = {
